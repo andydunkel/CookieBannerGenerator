ad3f16ff53a515842cb9f0239829db37969afb77
Andy Dunkel <andy.dunkel@ekiwi.de>
2022-04-10 17:54:15 +0200

    GUI and datamodel

--- src/udatamodel.pas
+++ src/udatamodel.pas
@@ -5,8 +5,7 @@ unit udatamodel;
 interface
 
 uses
-  Classes, SysUtils;
-
+  Classes, SysUtils, contnrs, XMLHelper, laz2_XMLRead, laz2_XmlWrite, laz2_DOM;
 
 type
   TGuiOptions = class
@@ -27,24 +26,42 @@ type
 
 type
   TToggle = class
+    public
+    isToggle: boolean;
     value: string;
     enabled: boolean;
     readonly: boolean;
   end;
 
+type
+  TCookieTableEntry = class
+  public
+    col1: String;
+    col2: String;
+    col3: String;
+    col4: String;
+    is_regex: boolean;
+  end;
 
 
 type
+
+  { TBlock }
+
   TBlock = class
     title: string;
     description: string;
     Toggle: TToggle;
-
-
+    CookieTable: TObjectList;
+    constructor Create;
   end;
 
 type
+
+  { TSettingsModal }
+
   TSettingsModal = class
+    public
     title: String;
     save_settings_btn: String;
     accept_all_btn: String;
@@ -54,15 +71,21 @@ type
     cookie_table_header2: String;
     cookie_table_header3: String;
     cookie_table_header4: String;
+    Blocks: TObjectList;
+    constructor Create;
   end;
 
 type
+
+  { TDataModel }
+
   TDataModel = class
   public
     current_lang: String;
     autoclear: boolean;
     cookie_name: String;
     page_scripts: boolean;
+    cookie_expiration: Integer;
     delay: Integer;
     hide_from_bots: boolean;
     remove_cookie_tables: boolean;
@@ -70,17 +93,66 @@ type
     GuiOptionSettingsModal: TGuiOptions;
     ConsentModal: TConsentModal;
     SettingsModal: TSettingsModal;
+
+    FileName : string;
+
+    procedure Save;
+    procedure Load;
+    constructor Create;
   end;
 
 
 implementation
 
-constructor TDataModel.Create
+{ TSettingsModal }
+
+constructor TSettingsModal.Create;
+begin
+  Self.Blocks:= TObjectList.Create;
+  Self.Blocks.OwnsObjects:= true;
+end;
+
+{ TBlock }
+
+constructor TBlock.Create;
+begin
+  Toggle:= TToggle.Create;
+  CookieTable:= TObjectList.Create;
+  CookieTable.OwnsObjects:= true;
+end;
+
+procedure TDataModel.Save;
+var
+  doc : TXMLDocument;
+  mainNode: TDOMNode;
+begin
+  doc:= TXMLDocument.Create;
+
+  mainNode:= TXMLHelper.CreateXmlNode(doc, 'xml');
+  doc.AppendChild(mainNode);
+
+  TXMLHelper.CreateXmlNode(doc, mainNode, 'current_lang', current_lang);
+  TXMLHelper.CreateXmlNode(doc, mainNode, 'cookie_name', cookie_name);
+  TXMLHelper.CreateXmlNode(doc, mainNode, 'autoclear', BoolToStr(autoclear));
+  TXMLHelper.CreateXmlNode(doc, mainNode, 'page_scripts', BoolToStr(page_scripts));
+  TXMLHelper.CreateXmlNode(doc, mainNode, 'hide_from_bots', BoolToStr(hide_from_bots));
+  TXMLHelper.CreateXmlNode(doc, mainNode, 'remove_cookie_tables', BoolToStr(remove_cookie_tables));
+  TXMLHelper.CreateXmlNode(doc, mainNode, 'delay', IntToStr(delay));
+
+  WriteXMLFile(doc, Self.FileName);
+end;
+
+procedure TDataModel.Load;
+begin
+
+end;
+
+constructor TDataModel.Create;
 begin
   Self.GuiOptionConsentModal:= TGuiOptions.Create;
   Self.GuiOptionSettingsModal:= TGuiOptions.Create;
   Self.ConsentModal:= TConsentModal.Create;
-  Self.SettingsModal:= TConsentModal.Create;
+  Self.SettingsModal:= TSettingsModal.Create;
 end;
 
 end.
--- src/umain.pas
+++ src/umain.pas
@@ -6,34 +6,53 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, Menus, ComCtrls,
-  ActnList;
+  ActnList, StdCtrls, udatamodel;
 
 type
 
   { TMainForm }
 
   TMainForm = class(TForm)
+    ActionSave: TAction;
     ActionNew: TAction;
     ActionOpen: TAction;
     ActionList1: TActionList;
+    cboLanguage: TComboBox;
+    chkHideBots: TCheckBox;
+    chkRemoveCookieTables: TCheckBox;
+    edtCookieName: TEdit;
+    edtDelay: TEdit;
+    GroupBox1: TGroupBox;
     ImageListToolbar: TImageList;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
     MainMenu: TMainMenu;
     MenuFile: TMenuItem;
     MenuExit: TMenuItem;
     MenuItem1: TMenuItem;
     MenuItem2: TMenuItem;
+    MenuItem3: TMenuItem;
     PageControl1: TPageControl;
+    SaveDialog: TSaveDialog;
     Separator1: TMenuItem;
     StatusBar1: TStatusBar;
     TabSheet1: TTabSheet;
     TabSheet2: TTabSheet;
     TabSheet3: TTabSheet;
     ToolBar1: TToolBar;
+    ToolButton1: TToolButton;
     ToolButtonNew: TToolButton;
     ToolButtonOpen: TToolButton;
     procedure ActionNewExecute(Sender: TObject);
+    procedure ActionSaveExecute(Sender: TObject);
+    procedure edtCookieNameChange(Sender: TObject);
+    procedure FormShow(Sender: TObject);
     procedure MenuExitClick(Sender: TObject);
   private
+    DataModel: TDataModel;
+    procedure DialogToModel;
+    procedure ModelToDialog;
 
   public
 
@@ -53,9 +72,41 @@ begin
   Application.Terminate;
 end;
 
+procedure TMainForm.DialogToModel;
+begin
+  DataModel.current_lang:= cboLanguage.Text;
+  DataModel.delay:= StrToInt(edtDelay.Text);
+
+end;
+
+procedure TMainForm.ModelToDialog;
+begin
+
+end;
+
 procedure TMainForm.ActionNewExecute(Sender: TObject);
 begin
 
+end;
+
+procedure TMainForm.ActionSaveExecute(Sender: TObject);
+begin
+  if SaveDialog.Execute then
+  begin
+    DialogToModel;
+    DataModel.FileName:= SaveDialog.FileName;
+    DataModel.Save;
+  end;
+end;
+
+procedure TMainForm.edtCookieNameChange(Sender: TObject);
+begin
+
+end;
+
+procedure TMainForm.FormShow(Sender: TObject);
+begin
+  DataModel:= TDataModel.Create;
 end;
 
 end.
